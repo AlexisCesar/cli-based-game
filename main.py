53897d0dbf3a28d042c3db4b3b195b339d51a4ef
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-04-24 20:47:32 -0300

    Added walls and improved print

--- main.py
+++ main.py
@@ -10,9 +10,31 @@ for i in range(default_map_size):
 	id_counter = id_counter + 1
 
 	for j in range (default_map_size):
-		layer.append(0)
+		layer.append(' ')
 	map.append(layer)
 
 for layer in map:
-	print(layer[1:-1])
+	if layer[0] == 0 or layer[0] == (default_map_size - 1):
+		layer_aux = list()
+		layer_aux.append(layer[0])
+		for i in range (default_map_size):
+			layer_aux.append('-')
+		map[map.index(layer)] = layer_aux
+	for index, column in enumerate(layer):
+		if index == 1 or index == (default_map_size):
+			layer[index] = '|'
+
+def printMap():
+	print('\n' * 100)
+	for layer in map:
+		for index, column in enumerate(layer[1:]):
+			print(column, end=' ')
+		print()
+
+
+map[(default_map_size - 2)][int(default_map_size / 2)] = 'X'
+
+while True:
+	printMap()
+	movement = input('waiting for command...')
 
