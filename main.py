0c8d089ce0d0ee8519b88f7e2509f9e5708a6e71
Alexis Cesar <alexis.cesar2017@outlook.com>
2022-04-29 15:59:06 -0300

    game map abstracted to generate_map function

--- main.py
+++ main.py
@@ -3,46 +3,51 @@ import os
 
 clearConsole = lambda: os.system('cls' if os.name=='nt' else 'clear')
 
-default_map_size = 20
+DEFAULT_MAP_SIZE = 20
 
-map = list()
+def generate_map():
+    map = list()
 
-id_counter = 0
-for i in range(default_map_size):
-    layer = list()
+    id_counter = 0
+    for i in range(DEFAULT_MAP_SIZE):
+        layer = list()
 
-    layer.append(id_counter)
-    id_counter = id_counter + 1
+        layer.append(id_counter)
+        id_counter = id_counter + 1
 
-    for j in range(default_map_size):
-        layer.append(' ')
-    map.append(layer)
+        for j in range(DEFAULT_MAP_SIZE):
+            layer.append(' ')
+        map.append(layer)
 
-for layer in map:
-    if layer[0] == 0 or layer[0] == (default_map_size - 1):
-        layer_aux = list()
-        layer_aux.append(layer[0])
-        for i in range(default_map_size):
-            layer_aux.append('-')
-        map[map.index(layer)] = layer_aux
-    for index, column in enumerate(layer):
-        if index == 1 or index == (default_map_size):
-            layer[index] = '|'
+    for layer in map:
+        if layer[0] == 0 or layer[0] == (DEFAULT_MAP_SIZE - 1):
+            layer_aux = list()
+            layer_aux.append(layer[0])
+            for i in range(DEFAULT_MAP_SIZE):
+                layer_aux.append('-')
+            map[map.index(layer)] = layer_aux
+        for index, column in enumerate(layer):
+            if index == 1 or index == (DEFAULT_MAP_SIZE):
+                layer[index] = '|'
+    return map
 
 
-def printMap():
+def print_map(map):
     clearConsole()
     for layer in map:
         for index, column in enumerate(layer[1:]):
             print(column, end=' ')
         print()
 
-player_column = player_row = 0
-player_column = int(default_map_size / 2)
-player_row = (default_map_size - 2)
-map[player_row][player_column] = 'X'
 
-printMap()
+game_map = generate_map()
+
+player_column = player_row = 0
+player_column = int(DEFAULT_MAP_SIZE / 2)
+player_row = (DEFAULT_MAP_SIZE - 2)
+game_map[player_row][player_column] = 'X'
+
+print_map(game_map)
 
 
 def on_press(key):
@@ -53,34 +58,34 @@ def on_press(key):
     except:
         k = key.name
     if k in ['w', 'a', 's', 'd']:
-        performMove(k)
+        performMove(k, game_map)
 
 
-def performMove(key):
+def performMove(key, game_map):
     global player_row
     global player_column
 
     if key == 'w':
-        if map[(player_row - 1)][(player_column)] == ' ':
-            map[player_row][player_column] = ' '
+        if game_map[(player_row - 1)][(player_column)] == ' ':
+            game_map[player_row][player_column] = ' '
             player_row = player_row - 1
-            map[player_row][player_column] = 'X'
+            game_map[player_row][player_column] = 'X'
     elif key == 'a':
-        if map[(player_row )][(player_column - 1)] == ' ':
-            map[player_row][player_column] = ' '
+        if game_map[(player_row )][(player_column - 1)] == ' ':
+            game_map[player_row][player_column] = ' '
             player_column = player_column - 1
-            map[player_row][player_column] = 'X'
+            game_map[player_row][player_column] = 'X'
     elif key == 's':
-        if map[(player_row + 1)][(player_column)] == ' ':
-            map[player_row][player_column] = ' '
+        if game_map[(player_row + 1)][(player_column)] == ' ':
+            game_map[player_row][player_column] = ' '
             player_row = player_row + 1
-            map[player_row][player_column] = 'X'
+            game_map[player_row][player_column] = 'X'
     elif key == 'd':
-        if map[(player_row )][(player_column + 1)] == ' ':
-            map[player_row][player_column] = ' '
+        if game_map[(player_row )][(player_column + 1)] == ' ':
+            game_map[player_row][player_column] = ' '
             player_column = player_column + 1
-            map[player_row][player_column] = 'X'
-    printMap()
+            game_map[player_row][player_column] = 'X'
+    print_map(game_map)
 
 
 with keyboard.Listener(on_press=on_press) as listener:
